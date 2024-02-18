e94298573ce2f9b6d64740714abf75fb15dbb70b
eefano <77832+eefano@users.noreply.github.com>
2024-02-18 23:35:13 +0100

    yo

---
+++ pumpupthejam.js
@@ -0,0 +1,31 @@
+// "Pump Up The Jam" (Technotronic) - Work In Progress
+const pickRestart = register('pickRestart', (arr, pat) => pat.pick(arr.map((x)=>x.restart(pat.collect().fmap(v=>v+1)))))
+const as = register('as', (mapping, pat) => { mapping = Array.isArray(mapping) ? mapping : [mapping];
+  return pat.fmap((v) => { v = Array.isArray(v) ? v : [v, 0];
+    return Object.fromEntries(mapping.map((prop, i) => [prop, v[i]])); }); });
+stack("~"
+,"<~@16 0@8>".pickRestart(
+  ["[ ~@2 4 [5:1 ~] ~ [~ 0] [3:-1@5 3:1@2 2]@2 ~ [4@3:1 3 3@3 2 2@3 3 4:1@3 0 0@2 2:2@2]@5 [~ ~ 0@2 ~ 0@2 -2:-3]@2 ]/4"
+]).as("n:penv").scale("c4:minor").legato(0.90).patt("0.1").s("square").delay(0.3).dfb(0.3).dt(60/128).gain(0.7)
+            
+,"<0@32>".pickRestart(
+  ["[~@13 [[~@3 [0,-2,-4]@2 ~]@3 [0,-2,-4] [1,-1,-3]!2]@3 ]/4"
+]).scale("c4:minor").note().legato(0.7).s("z_sawtooth").color("red").adsr("0.07:.1:0.6:0.1").gain(0.6)
+
+,"<0@12 0 1 ~@2 3@8>".pickRestart(
+  ["[0 ~@23]/2"
+  ,"~@2 [~ [e2 ~]] [[0 2] ~]"
+  ,"[0 ~ ~ 0 ~ ~ 0 ~] <[[~ [0 1]] [2 ~]] ~>"
+]).scale("c2:minor").note().legato(0.9).s("sawtooth").delay(0.6).dfb(0.5).dt(60/125*3/4).color("green").gain(1)
+
+,"<0@4 [0,1]@12 [0,1,2]@4 [0,1,2,3]@4>".pickRestart(
+ [stack(s("oh*16").pan(0.45).gain("[0.08 0.2]*4"),s("hh*4").pan(0.7).gain(0.30))
+ ,s("bd*4").lpf(120).gain(1)
+ ,s("[~ cp]*2").gain(0.6).pan(0.2)
+ ,s("[~ rd]*4").gain(0.15).release(0).hpf(1500).pan(0.8)
+ ,s("[~ sd!3]!4 [sd*4]!4").slow(2).gain(run(32).slow(2).mul(1/31).add(0.1).mul(0.4))
+ ,s("cr").gain(0.2)
+ ,s("bd").gain(0.8)
+ ]).bank("RolandTR909").color("yellow").velocity(0.7)
+ 
+).cpm(124/4).room(0.3)
