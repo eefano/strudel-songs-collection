c11eae3295775d0b5a785a2bf228d5cfe8e83e66
eefano <77832+eefano@users.noreply.github.com>
2024-02-20 23:33:11 +0100

    pieni gli alberghi a tunisi

--- cinghiale.js
+++ cinghiale.js
@@ -1,20 +1,18 @@
-const accordi   = x => x.note().s("recorder_tenor_sus").clip(1).release(0.5)
-const melodia   = x => x.note().s("gm_violin").gain(0.3).clip(1).release(0.5)
-const walking   = x => x.note().s("triangle").clip(1).release(0.1)
-const ritmo     = x => x.bank("YamahaRY30").clip(1).gain(0.08)
-const scaleggio = x => x.piano().gain(0.2)
-
-const myscale = "e:minor"
-
-const v1 = "[[2 1 0 1 2 0 1 2]!2 [4 3 2 3]*3@1.5 [4 ~!3]@0.5 ]/4"
-const v2 = "[[2 1 0 1 2 0 1 2]!2 [4 3 2 3]*3@1.5 [4 ~!3]@0.5 ]/4"
-
+// "L'Era Del Cinghiale Bianco" 
+// song @by Franco Battiato
+// script @by eefano
+const pickRestart = register('pickRestart', (arr, pat) => pat.pick(arr.map((x)=>x.restart(pat.collect().fmap(v=>v+1)))))
 stack(
-//"[0,2,4]/2".scale(myscale).apply(accordi),
-cat(v1,v2).add("<0 1 0 0>").scale(myscale).transpose(12).apply(melodia),
-//"~ -3 [0 ~] [0 ~] [1 ~] [1 ~] [2 3] [4 ~] ".scale(myscale).apply(walking),
-s("<[bd sd]>,hh*4").apply(ritmo),
-//s("hh!4").apply(ritmo)
-)
+  "<0@16 1@14 >".pickRestart(["<[0 1]!2 [0 2] [0 3]>/4","<4!2@7>"]).pickRestart(
+   ["<[7 6 5 6 7 5 6 7]!2>".add("<0 1>")
+   ,"<[9 8 7 8]*3@1.5 [9 ~!1]@0.5>"
+   ,"<9 [~ 7 8 6]>","<9 [~ 7 8 9]>"
+   ,"<7@3 6@3 7@5 8 9 7 5@3 6@3 7@5 ~@5>*4"
+  ]).scale("c4:major").add("<[0 -5]@16 [0@7 -5@8]!2@7 >").note().s("sawtooth").vib(5).vmod(0.1).clip(0.85).attack(0.05).release(0.1).gain(0.6)
 
-
+,"<0@16 1!2@7 >".pickRestart(["<[Am G C@2]!2 [Em D G@2]!2>/4","<Am@3 G@3 C@5 ~@3 Em@3 D@3 G@5 ~@5>*4"])
+  .chord().anchor("C4").voicing().s("triangle").lpf(1200).attack(0.01).gain(0.35)
+      
+,"<0@16 1@14 >".pickRestart(["[[bd <~!7 bd>] sd],<hh!31 oh>*4",
+                       "<bd [sd bd] bd bd [sd bd] bd sd>*2,<hh!17 oh>*4"]).s().bank("YamahaRY30").clip(1).gain(0.4)
+).cpm(127/2).room(0.4)
