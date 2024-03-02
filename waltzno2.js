18e4ffcf12c4572e47f03cdf8a8ba3cab04d57e3
eefano <77832+eefano@users.noreply.github.com>
2024-03-02 02:08:44 +0100

    waltz n.2

---
+++ waltzno2.js
@@ -0,0 +1,20 @@
+
+
+setcps(1)
+let tempo = sine.segment(16).slow(9)
+
+
+
+stack(cps(tempo.mul(20/60).add(120/60)).gain(0),
+
+"<4@3 [2@2 1]@3 0@4 0 1 2 0 2 4@2 5 4@3 3@3 3@3 [1@2 0]@3 b2@4 -3 b2 1 b2 1 3 4 5 f#3@3 4@3 >"
+      .scale("c:minor").transpose(12).note().s("gm_oboe:2").gain(0.7),
+      
+ n("<<0 -1> [4,5]!2>").chord("<Cm@6 Fm@4 G@4 Cm@2>/3")
+      .anchor('g2').mode('root').voicing().piano()
+     
+  
+ //,s("hh")
+   ).room(0.6)
+  //.clip("1".add(tempo))
+        .pianoroll()
