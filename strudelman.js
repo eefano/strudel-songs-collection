f330ca45a8a29b94e2626574edf309f55fe85c6e
eefano <77832+eefano@users.noreply.github.com>
2024-07-07 15:39:19 +0200

    he will eat you

---
+++ strudelman.js
@@ -0,0 +1,15 @@
+// "Strudelman will eat you"
+// @by eefano
+setcps(1)
+
+die: s("cr/16").late(666)
+  .bank(rand.range(0,3).pick(["RolandTR707","RolandS50","SequentialCircuitsDrumtracks","Linn9000"]))
+  .clip(rand.range(1,3))
+  .loop(1).loopBegin(.1)
+  .loopEnd(rand.range(.18,.3))
+  .speed(rand.range(.06,.10))
+  .pan(rand.range(.3,.7))
+  .gain(0.2).attack(.5).release(3).color('black')
+
+pain: s("<[RolandTR707_cr,RolandS50_cr,SequentialCircuitsDrumtracks_cr,Linn9000_cr] ~@666>").gain(0).color('black')
+
