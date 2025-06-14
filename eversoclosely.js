b913b6c8368efa539b66b7f17fcb4b43712cc8be
eefano <77832+eefano@users.noreply.github.com>
2025-06-14 22:40:21 +0200

    ever so remixed

--- eversoclosely.js
+++ eversoclosely.js
@@ -15,22 +15,21 @@ const mels ={0:"d4@2 a d4 g c4 f a d4"
             ,8:"[~@25 d4@29]/2"
             ,9:"<0,1,2,3,4,5,6,7>*9"}
 stack
-("<0 1 2 3 4 0 1 2 3 [4@26 ~] 0 1 2 3 [[4@25 ~@29],8]@2 >".pickRestart(mels).sub(12).note().s("gm_overdriven_guitar").gain(0.75).midichan(1)
-,"<~ ~ ~ ~ 5 5 6 7@2 [2@4 ~] 5 5 6 7 [[5@25 ~@29],8]@2>".pickRestart(mels).note().s("gm_tenor_sax").midichan(2).color("yellow")
-,"<1@14 [[4@25 ~@29],8]@2>".pickRestart(mels).add(24).note().s("gm_ocarina").gain(0.3).color("red")
-,"<9@14 ~@2>".pickRestart(mels).n().chord(epic).anchor("C4").voicing().s("<gm_reed_organ@10 gm_church_organ:1@4 ~@2>").gain("<0.25@10 0.4@4 ~@2>").color("green").midichan(3)
-,"<0@14 [[4@25 ~@29],8]@2>".pickRestart(mels).sub(24).note().s("gm_electric_bass_finger").lpf(300).gain(0.75).color("blue")
+("<0 1 2 3 4 0 1 2 3 [4@26 ~] 0 1 2 3 [[4@25 ~@29],8]@2 >".pickRestart(mels).sub(12).note().s("gm_overdriven_guitar").gain(.8)
+,"<~ ~ ~ ~ 5 5 6 7@2 [2@4 ~] 5 5 6 7 [[5@25 ~@29],8]@2>".pickRestart(mels).note().s("gm_tenor_sax").gain(.8).color("yellow")
+,"<1@14 [[4@25 ~@29],8]@2>".pickRestart(mels).add(24).note().s("gm_ocarina").gain(.3).color("red")
+,"<9@14 ~@2>".pickRestart(mels).n().chord(epic).anchor("C4").voicing().s("<gm_reed_organ@10 gm_church_organ:1@4 ~@2>").gain("<.25@10 .45@4 ~@2>").color("green").midichan(3)
+,"<0@14 [[4@25 ~@29],8]@2>".pickRestart(mels).sub(24).note().s("gm_electric_bass_finger").lpf(400).gain(.77).color("blue")
 ,"<0 [0@2 1@2 0 2@4] 0!4 [0@2 1@2 0 2@4] 0!2 [0@26 ~] 0!2 [0@2 1@2 0 2@4] 0 3@2>".pickRestart(
   ["<rd*3 , <<<bd!4 sd> sd> ~ bd>*3 , cr/9>*9"
   ,"[[sd [bd,cr] ~]!2@3]*9/2"
   ,"<[sd,[~ sd@10]] [[[bd,cr] ~]!2]@4 bd [sd,[~ sd@10]] mt lt >*9*3"
   ,"< [<[sd,[~ sd@10]]!2 ~> <[bd,cr]!2 [sd,[~ sd@10]]> [bd,cr]!2 ~ [bd,cr] ~ [bd,cr,cr] ~]*3 ~*4 >" 
-  ]).pickOut({rd:/*note(51).*/s('rd').velocity(0.1).pan(0.4),
-               bd:/*note(35).*/s('bd').velocity(0.5),
-                sd:/*note(38).*/s('sd').velocity(0.5),
-                cr:/*note(49).*/s('cr').velocity(0.1).pan(0.6),
-                mt:s('mt').velocity(0.4).pan(0.6),
-                lt:s('lt').velocity(0.4).pan(0.8)
-}).bank("Linn9000").gain(0.7).color("cyan").midichan(10)
-).room(0.6)//.midi()//.pianoroll()
+  ]).pickOut({rd:s('rd').velocity(.12).hpf(9000).pan(.45),
+               bd:s('bd').velocity(.5).lpf(2500),
+                sd:s('sd').velocity(.5).hpf(200),
+                cr:s('cr').velocity(.1).pan(.55),
+                mt:s('mt').velocity(.3).pan(.6),
+                lt:s('lt').velocity(.3).pan(.7)
+}).bank("Linn9000").speed(.95).gain(.7).color("cyan").midichan(10) ).room(.8)
  
