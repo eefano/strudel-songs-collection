e9fff06d34602ae2023ea3b0a9ff4a99106c6658
eefano <77832+eefano@users.noreply.github.com>
2025-06-27 01:36:48 +0200

    many markov tables

--- markovchain.js
+++ markovchain.js
@@ -1,25 +1,40 @@
 
-let markovstates = [0];
-                   //bd   sd    hh
-let markovtable = [[  0,  .2,  .8],  // bd
-                   [ .3,   0,  .7],  // sd
-                   [ .9,  .1,   0]]; // hh
+let markovstates = {};
+                   
+let markovtables = {
+  'drums':
+     //bd   sd    hh
+  [[  0,  .2,  .8],  // bd
+   [ .3,   0,  .7],  // sd
+   [ .9,  .1,   0]]  // hh
 
-const markov = register('markov', (pat) => pat.withHap((hap)=> {   
-     
+, 'chords':  
+  [[  .2,  .2,  .4,  .2],  
+   [ .5,   .3,  .2,  .1],
+   [  0,   .2,   .7,  .1],
+   [ .7,  .1,   .1,  .1]] 
+ }
+
+const markov = register('markov', (id, pat) => pat.withHap((hap)=> {   
+
+     if(markovstates[id]===undefined) markovstates[id]=[0];
+     const mystate = markovstates[id];
+     const mytable = markovtables[id];
+     //console.log(mytable)
+  
      const p = hap.whole.begin.n;
-     //console.log(p, markovstates.length)
-     while(markovstates.length<=p)
+     while(mystate.length<=p)
        {
-         const prev = markovstates[markovstates.length-1];
-         const t = markovtable[prev];
+         const prev = mystate[mystate.length-1];
+         const t = mytable[prev];
          let next = prev;
          for(let i=0,j=t[i];i<t.length;i++,j+=t[i]) if(hap.value<j) { next = i; break; }
-         markovstates.push(next);
-         //console.log(markovstates);
+         mystate.push(next);
+         //console.log(mystate);
        }
-     return hap.withValue((v)=>markovstates[p]);
+     return hap.withValue((v)=>mystate[p]);
 }))
 
-s(rand.segment(1).markov().pick(["bd","sd","hh"])).fast(8)
+$: s(rand.segment(1).markov('drums').pick(["bd","sd","hh"])).fast(8)
 
+$: chord(rand.late(.2).segment(1).markov('chords').pick(["C","Dm","F","A"])).fast(2).voicing().piano()
