9377324c749224c1cf3a866dd21d12f5e3743cc9
eefano <77832+eefano@users.noreply.github.com>
2025-11-22 21:27:13 +0100

    less warnings about arythmetics

--- bonespurs.js
+++ bonespurs.js
@@ -4,12 +4,12 @@
 setcps(90/60/3)
 
 $: "<[-3,0] [-4,0] [-2,0]>/4".layer(
-     x=>x.sub(7).n().s('supersaw').clip(1).lpf("<300>/12").lpa(0).lpe("<1 1.5 2 2!3>/12").lpd(.15).struct("<x*4>"),
-     x=>x.add(7).s('triangle').add(7).gain("<0 .1 .15 .2!3>/12").adsr([.5,0,1,.5]).hpf(500)
+     x=>x.sub(7).struct("<x*4>").n().s('supersaw').clip(1).lpf("<300>/12").lpa(0).lpe("<1 1.5 2 2!3>/12").lpd(.15),
+     x=>x.add(7).s('triangle').gain("<0 .1 .15 .2!3>/12").adsr([.5,0,1,.5]).hpf(500)
 )
 $: "<[0 2 5 0 3 -1 2 3 -1 -2 3 2]>".s('sine').gain("<0 0 .3 .6!3>/12")
 
-$: n(irand(7).struct("<~@2 [x x] [x ~]>").add(6)).s('square').hpf(800).lpf(2000)
+$: n(irand(7).add(6).struct("<~@2 [x x] [x ~]>")).s('square').hpf(800).lpf(2000)
   .gain(.3).adsr([.01,.2,.1,.4]).delay(0.2).dfb(.2).mask("<~@3 1!3>/12")
 
 $: s("[bd [~ <~ bd>] sd]").bank('linndrum').lpf("<600 800 1000 1200!3>/12").gain(.7)
