6804f213188421ecc9f1b15a83325cdf39e9b62e
olafbuitelaar <olaf.buitelaar@gmail.com>
2025-12-24 23:27:09 +0100

    Update AtDoomsGate.js
    added cycling harmonics

--- AtDoomsGate.js
+++ AtDoomsGate.js
@@ -1,35 +1,39 @@
 setcpm(115/4)
 
 $: stack(
-  // lead guitar
+  // DE LEAD GITAAR
   note(`<
     [e2 e2 e3 e2 e2 d3 e2 e2 c3 e2 e2 a#2 e2 e2 a#2 b2] [e2 e2 e3 e2 e2 d3 e2 e2 c3 e2 e2 a#2 - - - -] [e2 e2 e3 e2 e2 d3 e2 e2 c3 e2 e2 a#2 e2 e2 a#2 b2] [e2 e2 e3 e2 e2 d3 e2 e2 c3 e2 e2 a#2 - - - -]
     [e2 e2 e3 e2 e2 d3 e2 e2 c3 e2 e2 a#2 e2 e2 a#2 b2] [e2 e2 e3 e2 e2 d3 e2 e2 c3 e2 e2 a#2 - - - -] [e2 e2 e3 e2 e2 d3 e2 e2 c3 e2 e2 a#2 e2 e2 a#2 b2] [e2 - e2 - e3 - e2 - e2 - d3 - e2 - e2 - d5 c5 a4 [c5,c5] [e5,e5] [c5,c5] [b4,b4] [e5,e5,e5,e5] [a4,a4] [g#4,g#4] [c5,c5] [e5,e5] [c5,c5] [g#4,g#4] [g#4,g#4] -]
     [[g#4,g#4,e2] e2 e3 e2 e2 d3 e2 e2 c3 e2 e2 a#2 e2 e2 a#2 b2] [e2 e2 e3 e2 e2 d3 e2 e2 c3 e2 e2 a#2 - - - -] [e2 e2 e3 e2 e2 d3 e2 e2 c3 e2 e2 a#2 e2 e2 a#2 b2] [e2 e2 e3 e2 e2 d3 e2 e2 c3 e2 e2 a#2 - - - -]
     [e2 e2 e3 e2 e2 d3 [e2,e2] e2 c3 e2 [e2,e2] a#2 e2 e2 a#2 b2] [e2 e2 e3 e2 e2 d3 [e2,e2] e2 c3 e2 [e2,e2] a#2 - - - -] [e2 e2 e3 e2 e2 d3 e2 e2 c3 e2 e2 a#2 e2 e2 a#2 b2] [e2 - e2 - e3 - e2 - e2 - d3 - e2 - e2 - e5 [d5,d5] b4 a4 c5 e5 a5 e5 c5 a4 c5 [a5,a#4] e5 d5 c5 -]
     a4>`)
+    // De Transpositie: verspringt elke 4 maten
+    .add(note("<0 5 0 7>").slow(4)) 
+    // De Versnelling: gaat elke 4 maten iets sneller
+    .hurry("<1 1.05 1.07 1.09>".slow(4))
     .s("sawtooth")
-    .add(note(7))              
-    .distort(1.6)              
-    .decay(0.12).sustain(0)    
-    .gain(range(0.5, 0.7, rand))
-    .delay(rand.mul(0.005))    
-    .lpf(sine.range(900, 1600).slow(4)).lpq(15) 
-    .pan(range(-0.5, 0.5, sine.slow(8))), 
+    .add(note(7))              // Power chords
+    .distort(1.5)
+    .decay(0.12).sustain(0)
+    .gain(range(0.4, 0.6, rand))
+    .lpf(sine.range(1000, 2000).slow(8)).lpq(12),
 
-  // bass
+  // DE BAS - Volgt dezelfde transpositie en versnelling
   note(`<
     [c2,b1,e2,d2,c2,b1,e2,d2,c2,b1,d2,c2,b1,d2] [c2,b1,e2,d2,c2,b1,e2,d2,c2,b1,d2,c2,b1,d2] [c2,b1,e2,d2,c2,b1,e2,d2,c2,b1,d2,c2,b1,d2] [c2,b1,e2,d2,c2,b1,e2,d2,c2,b1,d2,c2,b1,d2]
     [c2,b1,e2,d2,c2,b1,e2,d2,c2,b1,d2,c2,b1,d2,c#3,c#3,c#3] [c2,b1,e2,d2,c2,b1,e2,d2,c2,b1,d2,c2,b1,d2,c#3,c#3,c#3] [c2,b1,e2,d2,c2,b1,e2,d2,c2,b1,d2,c2,b1,d2,c#3,c#3,c#3] [[c2,b1,e2,d2,c2,b1,e2,d2,c2,b1,d2,c2,b1,d2,c#3,c#3,c#3] - - - - - - - - - - - - - - - [e2,e2,e2,e2] - - - [e2,e2,e2,e2] - - - [e2,e2,e2,e2] - [e2,e2,e2,e2] - [e2,e2,e2,e2] [e2,e2,e2,e2] [e2,e2,e2,e2] -]
     [[e2,e2,e2,e2,c2,b1,c2,b1,c2,b1,c2,b1] [e2,e2,e2,e2,e2,e2,e2,e2] [c2,b1,c2,b1,c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1,c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1,c2,b1,c2,b1] [e2,e2,e2,e2]] [[c2,b1,c2,b1,c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1,c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1,c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1,c2,b1,c2,b1] [e2,e2,e2,e2]] [[c2,b1,c2,b1,c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1] [e2,e2,e2,e2]] [[c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1] [e2,e2,e2,e2] [e2,e2,e2,e2] [e2,e2,e2,e2] [e2,e2,e2,e2] [e2,e2,e2,e2]]
     [[c2,b1,c2,b1,c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1] [e2,e2,e2,e2]] [[c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1] [e2,e2,e2,e2]] [[c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1] [e2,e2,e2,e2]] [[c2,b1,c2,b1] [e2,e2,e2,e2] [c2,b1,c2,b1] [e2,e2,e2,e2] - - - -]>`)
+    .add(note("<0 5 0 7>").slow(4))
+    .hurry("<1 1.05 1.07 1.09>".slow(4))
     .s("triangle")
     .lpf(400)
-    .gain(0.8),
+    .gain(0.7),
 
-  // drums
+  // DRUMS
   stack(
     s("bd:4*2, [~ sd:2], hh27*16").gain(0.7),
-    s("bd:4(3,8)").gain(0.4) // Ghost notes
-  )
+    s("bd:4(3,8)").gain(0.4)
+  ).hurry("<1 1.05 1.07 1.09>".slow(4))
 )
