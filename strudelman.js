02de38e9e3acf3143b91fecc2a681e1135ce10f2
eefano <77832+eefano@users.noreply.github.com>
2026-05-26 21:15:06 +0200

    strudelman finetuned

--- strudelman.js
+++ strudelman.js
@@ -2,20 +2,20 @@
 // @by eefano
 setcps(1)
 
-die: s("cr/16").late(666)
-  .bank(rand.range(0,3).pick(["RolandTR707","RolandS50","SequentialCircuitsDrumtracks","Linn9000"]))
+die: s("cr/12").late(666)
+  .bank(irand(4).pick(["RolandTR707","RolandS50","SequentialCircuitsDrumtracks","Linn9000"]))
   .clip(rand.range(1,3))
   .loop(1).loopBegin(.1)
   .loopEnd(rand.range(.18,.3))
   .speed(rand.range(.06,.10))
-  .pan(rand.range(.3,.7))
+  .pan(rand.range(.4,.6))
   .gain(.2).attack(.5).release(3).color('black')
 
-doom: note("a1/8").late(333)
-  .s(rand.range(0,40).pick(["numbers:0","numbers:1","numbers:2","numbers:3","numbers:4",
-                            "numbers:5","numbers:6","numbers:7","numbers:8","numbers:9","~"]))
+doom: note("a1/6").late(333)
+  .s(irand(30).pick(["numbers:0","numbers:1","numbers:2","numbers:3","numbers:4",
+                     "numbers:5","numbers:6","numbers:7","numbers:8","numbers:9","~"]))
   .speed(rand.range(.2,.3))
-  .gain(.9).color('black')
+  .gain(.3).color('black')
 
 pain: s(`<[RolandTR707_cr,RolandS50_cr,SequentialCircuitsDrumtracks_cr,Linn9000_cr,
          numbers:0,numbers:1,numbers:2,numbers:3,numbers:4,
